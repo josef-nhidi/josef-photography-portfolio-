8202604fe1f267f18eebfb514fdc5c0440479c0a
Josef Nhidi <josef@nhidi.com>
2026-03-25 15:37:06 +0100

    Add missing Log facade import to AboutController

--- backend/app/Http/Controllers/Api/AboutController.php
+++ backend/app/Http/Controllers/Api/AboutController.php
@@ -7,6 +7,7 @@ use App\Models\AboutContent;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class AboutController extends Controller
