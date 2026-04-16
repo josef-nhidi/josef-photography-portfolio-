78547ea287c692f39aa4ab774c7a8798e0b225bd
Josef Nhidi <josef@nhidi.com>
2026-04-16 03:10:06 +0100

    feat: architectural remake - migrate to containerized deployment on Azure

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: false,
     rollupOptions: {
       output: {
         manualChunks(id) {
